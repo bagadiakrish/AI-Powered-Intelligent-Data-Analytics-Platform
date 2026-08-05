c3bbec49a6a3cf978f3bb66e681f4329a7ea891f
Krish Bagadia <bagadiakrish@gmail.com>
2026-08-06 00:42:14 +0530

    Fix: Configure ALLOWED_HOSTS to allow Render requests

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u^p^!o*=3dcejr$5w7r#)_whnf9(%!+0=98whr)tdux3^$+t&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
